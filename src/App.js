8587858fabaaf65dc51f0701e8693a1ab1791c6f
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-12-28 16:58:57 -0600

    Stripe simple frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
 import './App.css';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Stripe = React.lazy(() => import('./pages/Stripe'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback="loading">
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='/stripe' element={<Stripe />} />
+      </Routes>
+    </Suspense>
   );
 }
 
