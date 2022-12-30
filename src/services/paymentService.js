79f60283c8e23aa0ffa82e53a96eb83324c0d7a9
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2022-12-30 08:35:50 -0600

    resolved request method in payment intent

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -9,7 +9,7 @@ export default class PaymentService {
     }
     createPaymentIntent = ({productId,callbackSuccess,callbackError}) => {
         const requestInfo = {url:`${REACT_APP_BASE_URL}${REACT_APP_PAYMENT_ENDPOINT}/payment-intents?id=${productId}`,callbackSuccess,callbackError};
-        this.client.makeGetRequest(requestInfo);
+        this.client.makePostRequest(requestInfo);
     }
 
     pay = ({body,callbackSuccess,callbackError}) => {
